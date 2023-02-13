28ff82d29ff3f878439f1712f4c4e5e59dd2e347
Alvaro Cayo <96433864+albored@users.noreply.github.com>
2023-02-13 19:40:29 -0400

    cambiando max w a 2000

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import Footer from '../layout/Footer';
 import MainContainer from '../layout/MainContainer';
 import Header from '../layout/Header';
 import Main from '../layout/Main';
-import GridContainer from '../components/GridContainer';
+import GridContainer, { Categories } from '../components/GridContainer';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Main>
-        <Aside>{/* <Caregorias /> */}</Aside>
+        <Aside>
+          <Categories />
+        </Aside>
         <MainContainer>
           <GridContainer />
         </MainContainer>
