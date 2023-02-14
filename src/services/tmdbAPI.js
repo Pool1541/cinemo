ceef75a68cbaf8fb68b8d3910d23e431a5d129d3
Alvaro Cayo <96433864+albored@users.noreply.github.com>
2023-02-14 16:26:29 -0400

    creacion de lista de categorias y modificaicon css

--- src/services/tmdbAPI.js
+++ src/services/tmdbAPI.js
@@ -21,8 +21,25 @@ export const getGenres = async () => {
 };
 
 export const getPopular = async () => {
-  const response = await tmdbAPI.get(`
-  /popular?api_key=${apiKey}&language=en-US&page=1`);
+  const response = await tmdbAPI.get(
+    `/popular?api_key=${apiKey}&language=es-spa&page=1`
+  );
+
+  return response.data;
+};
+
+export const getTopRated = async () => {
+  const response = await tmdbAPI.get(
+    `/top_rated?api_key=${apiKey}&language=es-spa&page=1`
+  );
+
+  return response.data;
+};
+
+export const getSelectedList = async id => {
+  const response = await tmdbAPI.get(
+    `https://api.themoviedb.org/3/movie/${id}/lists?api_key=${apiKey}&language=en-US&page=1`
+  );
 
   return response.data;
 };
