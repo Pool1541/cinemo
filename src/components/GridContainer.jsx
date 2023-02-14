8fba67db36e7937d8906cd7a932b8ec59e8e9c24
Pool <pool_1541@hotmail.com>
2023-02-13 19:18:40 -0500

    GridContainer modificado

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,5 +1,7 @@
 import styles from '../styles/gridContainer.module.css';
 
 export default function GridContainer() {
-  return <div className={styles.gridContainer}>GRID CONTAINER MODIFICADO</div>;
+  return (
+    <div className={styles.gridContainer}>GRID CONTAINER MODIFICACION 2</div>
+  );
 }
