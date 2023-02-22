d478ae513eb70b4fb6f745e557539daf87c338ad
Alvaro Cayo <96433864+albored@users.noreply.github.com>
2023-02-22 13:57:22 -0400

    barra de navegacion fetch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,50 @@
 import styles from '../styles/components/navbar.module.css';
-import NavbarButton from './NavbarButton';
+import { useContext, useState } from 'react';
+import { DataContext } from '../contexts/dataContext';
+import { BiSearch } from 'react-icons/bi';
+import { getSearchResults } from '../services/tmdbAPI';
 
-function Navbar() {
+function NavbarButton() {
+  const { modal, setModal, setQueryValues } = useContext(DataContext);
+
+  const [text, setText] = useState('');
+
+  function handleClick() {
+    setModal(!modal);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setQueryValues({
+      fn: () => getSearchResults(text.replace(/ /g, '+')),
+      key: ['searchResults', text],
+    });
+    setText('');
+  }
+
   return (
-    <>
-      <NavbarButton />
-    </>
+    <div className={styles.navbar}>
+      <button onClick={handleClick} className={styles.navButton}>
+        <div className={styles.barra}></div>
+        <div className={styles.barra}></div>
+        <div className={styles.barra}></div>
+      </button>
+
+      <form onSubmit={handleSubmit} className={styles.searchBar}>
+        <input
+          onChange={e => setText(e.target.value)}
+          value={text}
+          type='text'
+          className={styles.search}
+        />
+        <button type='submit' className={styles.btnSearch}>
+          <BiSearch />
+        </button>
+      </form>
+
+      <span>login</span>
+    </div>
   );
 }
 
-export default Navbar;
+export default NavbarButton;
