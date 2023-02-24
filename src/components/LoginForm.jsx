bcf04c16289f9efba833cda3c384529e858b481a
Pool <pool_1541@hotmail.com>
2023-02-23 20:10:58 -0500

    inicio de sesion y registro agregado

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,6 @@
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { lazy, Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
 import { auth } from 'services/firebase';
 import styles from 'styles/components/loginForm.module.css';
 
@@ -13,19 +15,48 @@ export default function LoginForm() {
     }
   }
 
+  function handleRegisterModal() {
+    const Modal = lazy(() => import('./RegisterModal'));
+    const background = document.createElement('div');
+    background.id = styles.background;
+    const root = createRoot(
+      document.querySelector('#root').appendChild(background)
+    );
+    root.render(
+      <Suspense fallback={<div>Loading...</div>}>
+        <Modal root={root} id={styles.background} />
+      </Suspense>
+    );
+  }
+
   return (
     <div className={styles.loginContainer}>
-      <form className={styles.form}>
-        <label htmlFor='user'>Usuario</label>
-        <input type='text' id='user' />
-        <label htmlFor='password'>Contraseña</label>
-        <input type='text' id='password' />
-        <button type='button'>Iniciar sesion</button>
-        <button type='button'>Registrate</button>
-        <button type='button' onClick={loginWithGoogle}>
-          Ingresar con Google
-        </button>
+      <form className={styles.formBox}>
+        <div className={styles.title}>
+          <h1>Iniciar sesión</h1>
+        </div>
+        <div className={styles.inputs}>
+          <label htmlFor='loginEmail'>Correo electrónico</label>
+          <input type='text' id='loginEmail' />
+          <label htmlFor='loginPassword'>Contraseña</label>
+          <input type='password' id='loginPassword' />
+        </div>
+        <div className={styles.registered}>
+          <p>
+            ¿Aún no tienes una cuenta?{' '}
+            <a onClick={handleRegisterModal}>Registrate</a>
+          </p>
+        </div>
+        <div className={styles.buttons}>
+          <button type='button'>Iniciar sesion</button>
+          <button type='button' onClick={loginWithGoogle}>
+            Ingresar con Google
+          </button>
+        </div>
       </form>
+      <div className={styles.imageBox}>
+        <img src={require('assets/banner.png')} alt='movies banner' />
+      </div>
     </div>
   );
 }
